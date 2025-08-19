759b2b14829795f9257df4fffad51534498e0fac
Andrii Stavskyi <an.stawski@outlook.com>
2025-08-19 19:51:21 +0300

    refactor: enhance docs pages with htmx

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,8 +1,13 @@
-document.querySelectorAll('.soc-code-toggle').forEach(button => {
-    button.addEventListener('click', () => {
-        const codeBlock = button.nextElementSibling;
-        const isVisible = codeBlock.style.display === 'block';
-        codeBlock.style.display = isVisible ? 'none' : 'block';
-        button.textContent = isVisible ? 'Show code' : 'Hide code';
+function bindCodeToggles() {
+    document.querySelectorAll('.soc-code-toggle').forEach(button => {
+        button.onclick = () => {
+            const codeBlock = button.nextElementSibling;
+            const isVisible = codeBlock.style.display === 'block';
+            codeBlock.style.display = isVisible ? 'none' : 'block';
+            button.textContent = isVisible ? 'Show code' : 'Hide code';
+        };
     });
-});
+}
+
+bindCodeToggles();
+document.body.addEventListener('htmx:afterSwap', bindCodeToggles);
